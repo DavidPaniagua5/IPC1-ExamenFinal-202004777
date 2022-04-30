a4dc0e378c00924984488aa1d3f364265b0c5603
DavidPaniagua5 <2998578320101@ingenieria.usac.edu.gt>
2022-04-29 22:28:56 -0600

    Primer commit

---
+++ Final/src/main/java/Final/Main.java
@@ -0,0 +1,11 @@
+package Final;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new scanner(System.in);
+    public static void main(String[] args) {
+        
+        System.out.println("***********************");
+        System.out.println("1. ");
+    }
+}
